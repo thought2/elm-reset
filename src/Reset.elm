a53f5206b8de9da21479a8e016dc1007c9b82d60
Michael Bock <me@thought2.de>
2019-02-06 21:00:39 +0100

    add docs

--- src/Reset.elm
+++ src/Reset.elm
@@ -1,37 +1,52 @@
 module Reset exposing (..)
 
+{-|
 
+@docs List, Tuple, Maybe, Result, Int, String, Char, Float, Bool
+
+-}
+
+
+{-| -}
 type List
     = Reset1
 
 
+{-| -}
 type Tuple
     = Reset2
 
 
+{-| -}
 type Maybe
     = Reset3
 
 
+{-| -}
 type Result
     = Reset4
 
 
+{-| -}
 type Int
     = Reset5
 
 
+{-| -}
 type String
     = Reset6
 
 
+{-| -}
 type Char
     = Reset7
 
 
+{-| -}
 type Float
     = Reset8
 
 
+{-| -}
 type Bool
     = Reset9
